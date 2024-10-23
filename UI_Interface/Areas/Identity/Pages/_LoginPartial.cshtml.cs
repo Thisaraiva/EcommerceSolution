6f4b9f5d744ee3c12c62783631bca1293a67eb5b
Thisaraiva <thiagofreitassaraiva@yahoo.com.br>
2024-10-23 13:06:09 -0300

    Identity feature dependency changes for ApplicationUser

--- UI_Interface/Areas/Identity/Pages/_LoginPartial.cshtml.cs
+++ UI_Interface/Areas/Identity/Pages/_LoginPartial.cshtml.cs
@@ -1,5 +1,8 @@
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
 
 namespace UI_Interface.Areas.Identity.Pages
 {
@@ -7,6 +10,27 @@ namespace UI_Interface.Areas.Identity.Pages
     {
         public void OnGet()
         {
+        }
+
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public _LoginPartialModel(SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        public ApplicationUser CurrentUser { get; set; }
+        public bool IsSignedIn { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            IsSignedIn = _signInManager.IsSignedIn(User);
+            if (IsSignedIn)
+            {
+                CurrentUser = await _userManager.GetUserAsync(User);
+            }
         }
     }
 }
